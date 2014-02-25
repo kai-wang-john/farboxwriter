77f98f0f7397239fd91f4d27900695e3e186a2bc
Hepo <hepochen@gmail.com>
2014-02-25 15:15:11 +0800

    no need to compile scss and modify the scripts.js to relative url path

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -89,7 +89,8 @@ jQuery(function($) {
 
             // If the requested url is not the current states url push
             // the new state and make the ajax call.
-            if (url !== currentState.url.replace(/\/$/, "")) {
+            // currentState.url.replace(/\/$/, ""), use relative path instead, sometimes, both http & https work together.
+            if (url !== currentState.url.replace(/https?:\/\/.*?\//, "/")) {
                 loading = true;
 
                 // Check if we need to show the post index after we've
